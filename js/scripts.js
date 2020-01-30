2a35793744536a135a428a65e2ffbdd51b83e2d1
AustinOlig <austinoligario@gmail.com>
2020-01-30 01:51:55 -0500

    Refactored index.html into new.html with bulma.io css framework

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //file: scripts.js!!!!
 var music = document.getElementById("bg-music");
 var mute = document.getElementById("mute-button");
-music.volume = 0.25;
+music.volume = 0.1;
 
 window.onload = function() {   
   music.muted = false;
@@ -13,9 +13,22 @@ function isPlaying(music) { return !music.paused; }
 function toggleMusic() {
   if (isPlaying(music)) {
       music.pause();
-      mute.innerHTML = "Unmute";
+      mute.innerHTML = "<span class=\"icon\"><i class=\"fas fa-volume-off\"></i></span>";
   } else {      
       music.play();
-      mute.innerHTML = "Mute";
+      mute.innerHTML = "<span class=\"icon\"><i class=\"fas fa-volume-up\"></i></span>";
   }
 }
+
+/* CSCI 3230U - CSS Frameworks */
+$(document).ready(function() {
+
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function() {
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+
+  });
+});
