e5c5dc3e1fa913deaa27c437862979c32f992285
AustinOlig <austinoligario@gmail.com>
2020-02-14 00:55:22 -0500

    temporarily added project files for live demo

---
+++ js/website.js
@@ -0,0 +1,30 @@
+window.onload = function(){
+    document.getElementById('main-content').style.display = "initial";
+}
+
+function openTab(evt, tabContentName) {
+    var i, tabContents, tabs, hero;
+    tabContents = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContents.length; i++) {
+        tabContents[i].style.display = "none";
+    }
+
+    tabs = document.getElementsByClassName("tab");
+    for (i = 0; i < tabContents.length; i++) {
+        tabs[i].className = tabs[i].className.replace(" is-active", "");
+    }
+
+    evt.currentTarget.className += " is-active";
+    document.getElementById(tabContentName).style.display = "initial";
+
+    hero = document.getElementById('hero');
+    if (hero.className.includes('is-info')) {
+        hero.className = hero.className.replace("is-info", "is-primary");
+    } else if (hero.className.includes('is-primary')) {
+        hero.className = hero.className.replace("is-primary", "is-success");
+    } else if (hero.className.includes('is-success')) {
+        hero.className = hero.className.replace("is-success", "is-warning");
+    } else if (hero.className.includes('is-warning')) {
+        hero.className = hero.className.replace("is-warning", "is-info");
+    } 
+}
